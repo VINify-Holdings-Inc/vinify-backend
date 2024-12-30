e242b9c67c4f3342c80bf1e924af58416eece71e
amit9792 <amit.chauhan@techwagger.com>
2024-12-30 10:13:10 +0530

    new

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,7 +1,7 @@
  
 import express from "express";
 import { deleteContactUs, insertContactUs, readContactUs } from "../controller/ContactUs";
-import { getBulkSheetData, getBulkSheetDataSheet2, insertBulkSheetData, insertBulkSheetDatSheet2 } from "../controller/BulkInsert";
+import { getBulkSheetData, getSearchVinPop, getBulkSheetDataSheet2, insertBulkSheetData, insertBulkSheetDatSheet2 } from "../controller/BulkInsert";
 import { ForgetPassword, LoginController, ResetPassword, userProfileUpdate , ProfileUpdate, ResetTockenCheck} from "../controller/LoginController";
 const routerAdmin = express.Router();
 
@@ -20,5 +20,5 @@ routerAdmin.post("/csv-import", insertBulkSheetData);
 routerAdmin.get("/csv-import",  getBulkSheetData); 
 routerAdmin.post("/csv-import-sheet2", insertBulkSheetDatSheet2);
 routerAdmin.get("/csv-import-sheet2",  getBulkSheetDataSheet2);
-
+routerAdmin.get("/search-pop-vin/:vin",  getSearchVinPop);
 export default routerAdmin;
